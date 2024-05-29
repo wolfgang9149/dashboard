969c1eba64abf779131c8f95abfffd8240bc006a
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2024-05-29 21:08:18 +1000

    Initial setup React Vite App (#2)
    * setup eslint and prettier
    
    * setup CI/CD
    
    * wip
    
    * wip
    
    * setup tailwindcss
    
    * delete unnecessary files

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    "plugin:import/errors",
+    "prettier"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', "prettier"],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "import/order": "error",
+    "no-duplicate-imports": "error",
+    "react/jsx-indent": [
+      1,
+      2
+    ],
+    "no-console": "error",
+    "no-unused-vars": "error",
+    "prettier/prettier": "warn",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ]
+  },
+  "env": {
+    "es6": true,
+    "jest": true,
+    "browser": true
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx"]
+      }
+    }
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className='p-4 text-pink-500'>Vite + React + Tailwindcss</h1>;
+}
+
+export default App;
