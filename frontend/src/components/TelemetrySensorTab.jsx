0328adcbaae6a963db6f0158a7bf97f4bc5ec08f
Thomas Loo <tomloo222@gmail.com>
2024-06-03 14:40:08 +1000

    Hook sensor data from backend API to sensor components

--- frontend/src/components/TelemetrySensorTab.jsx
+++ frontend/src/components/TelemetrySensorTab.jsx
@@ -1,23 +1,25 @@
 
 
 
-export default function TelemetrySensorTab() {
+export default function TelemetrySensorTab({ teleSensor }) {
 
-
+    if (!teleSensor) {
+        return <div>Telemetry data loading</div>
+    }
 
     return (
         <div className="bg-white p-3 rounded-xl border-4 border-solid border-black w-[24rem] mt-2">
             <div className="flex flex-row justify-between">
                 <p className="text-2xl pl-2">X-axis</p>
-                <p className="text-2xl pr-2">0.141357</p>
+                <p className="text-2xl pr-2">{teleSensor.AXC.toFixed(6)}</p>
             </div>
             <div className="flex flex-row justify-between">
                 <p className="text-2xl pl-2">Y-axis</p>
-                <p className="text-2xl pl-2">-0.003662</p>
+                <p className="text-2xl pl-2">{teleSensor.AXY.toFixed(6)}</p>
             </div>
             <div className="flex flex-row justify-between">
                 <p className="text-2xl pl-2">Z-axis</p>
-                <p className="text-2xl pl-2">-8</p>
+                <p className="text-2xl pl-2">{teleSensor.AXZ.toFixed(6)}</p>
             </div>
         </div>
     )
